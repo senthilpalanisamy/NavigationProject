ed9818fdf32f80ca96b749d45fe6e8e5af0dce73
Senthil Palanisamy <senthillihtnes1994@gmail.com>
2020-01-12 12:19:42 -0600

    Completed part B.001
    - The turtle follows a rectangular path in a feed forward control

--- tsim/include/turtle_rect.hpp
+++ tsim/include/turtle_rect.hpp
@@ -1,12 +1,23 @@
 #include<iostream>
 #include<ros/ros.h>
 #include<ros/console.h>
+#include<cmath>
 #include<geometry_msgs/Twist.h> 
 #include<turtlesim/SetPen.h>
 #include<turtlesim/TeleportAbsolute.h>
 
 namespace Turtle_Navigation
 {
+  enum Turtle_States {INITIALISE, FOLLOW_WAYPOINT, CHANGE_WAY_POINT,
+                      RESET};
+
+  struct Circular_Linked_List
+  {
+    Circular_Linked_List(Circular_Linked_List* previous_element, int x, int y);
+    int x, y;
+    Circular_Linked_List* next_element;
+  };
+
   class Rect_Navigation
   {
     public:
@@ -14,10 +25,22 @@ namespace Turtle_Navigation
       void restart_trajectory();
       void pose_callback();
       void execute_trajectory();
+      void follow_way_point();
+      Circular_Linked_List* current_waypoint;
+      //Circular_Linked_List current_element;
+      Turtle_States state;
+      double current_x, current_y, goal_angle, current_angle;
+      double total_time;
+      float frequency;
+      geometry_msgs::Twist previous_velocity;
+      ros::Time previous_time;
     private:
-      int height, width, x, y;
-      float frequency, rot_vel, trans_vel;
+      float height, width, x, y;
+      float rot_vel, trans_vel;
       void initialise_turtle();
+      void update_current_pose();
+      Circular_Linked_List* create_waypoints_list();
+      ros::Publisher velocity_publisher;
   };
 
 }
--- tsim/src/turtle_rect.cpp
+++ tsim/src/turtle_rect.cpp
@@ -4,6 +4,21 @@ using namespace std;
 
 namespace Turtle_Navigation
 {
+ Circular_Linked_List::Circular_Linked_List(Circular_Linked_List* previous_element, 
+                                            int x_value, int y_value)
+ {
+   x =  x_value;
+   y = y_value;
+   if(previous_element != NULL)
+   {
+     next_element = previous_element->next_element;
+     previous_element-> next_element = this;
+   }
+   else
+   {
+     next_element = this;
+   }
+ }
 
 Rect_Navigation::Rect_Navigation(int argc, char **argv)
 {
@@ -21,11 +36,22 @@ Rect_Navigation::Rect_Navigation(int argc, char **argv)
   cout<<height<<"\n"<<width<<"\n"<<x<<"\n"<<y<<"\n"<<frequency<<"\n"<<rot_vel<<"\n"<<trans_vel<<"\n";
   ROS_INFO_STREAM("\n height:" <<height<<"\nwidth:" <<width <<"\nx"<<x<<"\ny"<<y
                   <<"\nfrequency:"<<frequency<<"\nrotational velocity"<<rot_vel<<"\ntranslational velocity"<<trans_vel);
-  ros::Rate loop_rate(frequency);
-  initialise_turtle();
-  // ros::Rate loop_rate(10.0);
-
+  state = INITIALISE;
+  current_waypoint = create_waypoints_list();
+  velocity_publisher = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
+  total_time = 0;
 }
+
+Circular_Linked_List* Rect_Navigation::create_waypoints_list()
+    {
+
+    Circular_Linked_List* head = new Circular_Linked_List(NULL, x, y);
+    Circular_Linked_List* element2 = new Circular_Linked_List(head, x+width, y);
+    Circular_Linked_List* element3 = new Circular_Linked_List(element2, x+width, y+height);
+    Circular_Linked_List* element4 = new Circular_Linked_List(element3, x, y+height);
+    return element2;
+    }
+
 
 void Rect_Navigation::initialise_turtle()
 {
@@ -50,14 +76,98 @@ void Rect_Navigation::initialise_turtle()
    pen.request.g = 0;
    pen.request.b = 0;
    pen.request.width = 5;
+   pen.request.off=0;
    Turtle1_Set_Pen.call(pen);
+   state = FOLLOW_WAYPOINT;
+
+   goal_angle = 0;
+   current_angle = 0;
+   previous_time = ros::Time::now();
 
 }
 
+void Rect_Navigation::update_current_pose()
+{
+  ros::Duration time_duration = ros::Time::now() - previous_time;
+  double time_elapsed = time_duration.toSec();
+  total_time += time_elapsed;
+  ROS_INFO_STREAM("time elapsed:  "<<time_elapsed);
+  current_angle += time_elapsed * previous_velocity.angular.z;
+  current_x += time_elapsed * previous_velocity.linear.x * cos(current_angle);
+  current_y += time_elapsed * previous_velocity.linear.x * sin(current_angle);
+  ROS_INFO_STREAM("current_x:  " << current_x <<"current_y:  " <<current_y<<"current theta:  "<<current_angle);
+                 //<<"previous_time"<<previous_time<<"current_time"<<ros::Time::now()
+                 //<<"total_time"<<total_time<<"\n sine theta"<<sin(current_angle));
+  current_angle = atan2(sin(current_angle), cos(current_angle));
+}
+
+void Rect_Navigation::follow_way_point()
+{
+  geometry_msgs::Twist velocity_message;
+  update_current_pose();
+  float goal_angle = atan2(current_waypoint->y - current_y, current_waypoint->x - current_x);
+  float angle_difference = current_angle - goal_angle;
+  float angle_diff_wrapped = atan2(sin(angle_difference), cos(angle_difference));
+  float distance_difference = pow(pow((current_waypoint->x - current_x), 2) +
+                              pow((current_waypoint->y - current_y), 2), 0.5);
+
+  if(abs(angle_diff_wrapped) >= 0.1)
+  {
+    ROS_INFO_STREAM("angle difference" << current_angle - goal_angle);
+     if (angle_diff_wrapped > 0)
+       velocity_message.angular.z =  -rot_vel;
+     else
+      velocity_message.angular.z =  rot_vel;
+    velocity_publisher.publish(velocity_message);
+  } 
+  else if (distance_difference > 0.1)
+  {
+
+    ROS_INFO_STREAM("distance difference" <<pow(pow((current_waypoint->x - current_x), 2) +
+                                             pow((current_waypoint->y - current_y), 2), 0.5));
+    velocity_message.linear.x = trans_vel;
+    velocity_publisher.publish(velocity_message);
+  } 
+  else
+  {
+    ROS_INFO_STREAM("changing way point");
+    state = CHANGE_WAY_POINT;
+  }
+  previous_time = ros::Time::now();
+  previous_velocity = velocity_message;
+}
+ 
+
+void Rect_Navigation::execute_trajectory()
+{
+  switch(state)
+  {
+  case INITIALISE      :initialise_turtle();
+                        break;
+  case FOLLOW_WAYPOINT :follow_way_point();
+                        break;
+  case CHANGE_WAY_POINT:current_waypoint = current_waypoint->next_element;
+                        //ROS_INFO_STREAM(current_waypoint)
+                        // if(goal_angle >= 1.5)
+                        //   goal_angle = 0;
+                        // else
+                        //   goal_angle = 1.5708;
+                        state = FOLLOW_WAYPOINT;
+                        break;
+
+}
+
+}
 }
 
 int main(int argc, char **argv){
   Turtle_Navigation::Rect_Navigation Rect_Turtle(argc, argv);
+  ros::Rate loop_rate(Rect_Turtle.frequency);
+  while (ros::ok())
+  {
+  Rect_Turtle.execute_trajectory();
+  loop_rate.sleep();
+  }
 
   //ros::init(argc, argv, "turtle_rect");
   //int height, width, x, y;
