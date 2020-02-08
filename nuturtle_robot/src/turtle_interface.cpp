ad35f2194ae0fd0cbe8dd2fb97a6498cddc27c33
Senthil Palanisamy <senthillihtnes1994@gmail.com>
2020-02-07 19:02:19 -0600

    Added a subscriber for publishing joint states of the turtlebot
    - This subscriber will publish the joint states based on Senor data
      from turtlebot

--- nuturtle_robot/src/turtle_interface.cpp
+++ nuturtle_robot/src/turtle_interface.cpp
@@ -1,7 +1,9 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
+#include <sensor_msgs/JointState.h>
 #include "rigid2d/rigid2d.hpp"
 #include "nuturtlebot/WheelCommands.h" 
+#include "nuturtlebot/SensorData.h" 
 
 double clampValue(double input, double upperLimit, double lowerLimit)
 {
@@ -22,12 +24,17 @@ double clampValue(double input, double upperLimit, double lowerLimit)
 class turtleInterface
 {
 
-  ros::Subscriber cmdVelSubscriber;
-  ros::Publisher wheelCommandPublisher;
+  ros::Subscriber cmdVelSubscriber, sensorDataSubscriber;
+  ros::Publisher wheelCommandPublisher, jspPublisher;
   double maxMotorVelocity, robotMaxTransVel, robotMaxRotVel;
   double wheelBase, wheelRadius;
   rigid2d::DiffDrive diffcar;
   double maxWheelCommand, minWheelCommand, scaleMotorVelToCmd;
+  std::string leftWheelJoint, rightWheelJoint;
+  bool bIsFirstRun;
+  int encoderTicksPerRevolution;
+  float leftPrevPosition, rightPrevPosition;
+  ros::Time currentTime, lastTime;
   
   public:
   turtleInterface(int argc, char **argv)
@@ -40,19 +47,28 @@ class turtleInterface
   cmdVelSubscriber = n.subscribe("/cmd_vel", 1000, &turtleInterface::cmdVelCallback,
                                   this);
 
+  sensorDataSubscriber = n.subscribe("/sensor_data", 1000, &turtleInterface::sensorDataCallback,
+                                  this);
   wheelCommandPublisher = n.advertise<nuturtlebot::WheelCommands>("/wheel_commands", 1000);
+  //jspPublisher = n.advertise<sensor_msgs::JointState>("/joint_states", 1000);
+  jspPublisher = n.advertise<sensor_msgs::JointState>("/jsp", 1000);
 
   ros::param::get("max_rot_vel_motor", maxMotorVelocity);
   ros::param::get("max_trans_vel", robotMaxTransVel);
   ros::param::get("max_rot_vel_motor", robotMaxRotVel);
   ros::param::get("wheel_base", wheelBase);
   ros::param::get("wheel_radius", wheelRadius);
+  ros::param::get("~left_wheel_joint", leftWheelJoint);
+  ros::param::get("~right_wheel_joint", rightWheelJoint);
+  ros::param::get("encoder_ticks_per_rev", encoderTicksPerRevolution);
+
 
   rigid2d::Transform2D identityTransform(0);
   diffcar = rigid2d::DiffDrive(identityTransform, wheelBase, wheelRadius); 
   maxWheelCommand = 44;
   minWheelCommand = -44;
   scaleMotorVelToCmd = (maxWheelCommand - minWheelCommand) / (2  * maxMotorVelocity);
+  bIsFirstRun = true;
   }
 
 void cmdVelCallback(const geometry_msgs::Twist bodyTwistMsg)
@@ -73,6 +89,56 @@ void cmdVelCallback(const geometry_msgs::Twist bodyTwistMsg)
     wheelVelCommands.left_velocity = velocities.left * scaleMotorVelToCmd;
     wheelVelCommands.right_velocity = velocities.right * scaleMotorVelToCmd;
     wheelCommandPublisher.publish(wheelVelCommands);
+}
+
+void sensorDataCallback(const nuturtlebot::SensorData turtlebotSensor)
+{
+
+    currentTime = ros::Time::now();
+    sensor_msgs::JointState jointStateMsg;
+    jointStateMsg.header.stamp = currentTime;
+    jointStateMsg.header.frame_id = "turtle_interface";
+
+
+
+    std::vector<std::string> jointNames = {leftWheelJoint, rightWheelJoint};
+    std::vector<double> jointPositions;
+    std::vector<double> jointVelocities;
+
+
+
+    if(bIsFirstRun)
+    {
+      jointPositions = {0.0, 0.0};
+      bIsFirstRun = false;
+    }
+
+    jointPositions = {turtlebotSensor.left_encoder / float(encoderTicksPerRevolution),
+                     turtlebotSensor.right_encoder / float(encoderTicksPerRevolution)};
+    if(bIsFirstRun)
+    {
+      jointVelocities = {0.0, 0.0};
+    }
+    else
+    {
+
+    ros::Duration totalDuration = currentTime - lastTime;
+    double totalTime = totalDuration.toSec();
+    jointVelocities = {(jointPositions[0] - leftPrevPosition) / totalTime,
+                     (jointPositions[0] - rightPrevPosition) / totalTime};
+    }
+
+
+
+    jointStateMsg.name = jointNames;
+    jointStateMsg.position = jointPositions;
+    jointStateMsg.velocity = jointVelocities;
+    jspPublisher.publish(jointStateMsg);
+
+    lastTime = currentTime;
+    leftPrevPosition = jointPositions[0];
+    rightPrevPosition = jointPositions[1];
+
 }
 };
 
