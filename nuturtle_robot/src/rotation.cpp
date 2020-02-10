acac066a32cfbec8e54dfcbcff0ac77e1ee15635
Senthil Palanisamy <senthillihtnes1994@gmail.com>
2020-02-09 19:30:00 -0600

    Added a node for rotating in place
    - This node will publish cmd_vel commands so that the turtelbot
      rotates in place

--- nuturtle_robot/src/rotation.cpp
+++ nuturtle_robot/src/rotation.cpp
@@ -1,13 +1,23 @@
 #include <ros/ros.h>
 #include <ros/console.h>
 #include <nuturtle_robot/StartRotation.h>
-#include <rigid2d/SetPose.h> 
+#include "rigid2d/SetPose.h"
+#include <geometry_msgs/Twist.h>
+#include "rigid2d/rigid2d.hpp"
+
+constexpr double callTime=0.01;
 
 class RotateInPlace
 {
   ros::ServiceServer startRotation;
   ros::ServiceClient setPose;
-  bool isClockwise;
+  ros::Publisher cmdVelPUblisher;
+  bool isClockwise, isWaitTime;
+  double totalElapsedTime, rotationVelocity, callbackTime, rotationPeriod;
+  int rotationCount, callbackCount, rotationsNeeded;
+  ros::Time previousTime;
+  enum RotationStates {ROTATE, WAIT, STOP}; 
+  RotationStates state;
   public:
   RotateInPlace(int argc, char** argv)
   {
@@ -17,8 +27,17 @@ class RotateInPlace
 
     startRotation = n.advertiseService("/start", &RotateInPlace::startCallback,
                                      this);
+    cmdVelPUblisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
     setPose = n.serviceClient<rigid2d::SetPose>("/set_pose");
     isClockwise = true;
+    callbackTime = callTime;
+    totalElapsedTime = 0.0;
+    rotationCount = 0;
+    rotationVelocity = 1.0;
+    isWaitTime = false;
+    state = ROTATE;
+    rotationPeriod = 2 * rigid2d::PI / rotationVelocity;
+    rotationsNeeded = 2;
   }
 
   bool startCallback(nuturtle_robot::StartRotation::Request& request,
@@ -37,13 +56,67 @@ class RotateInPlace
 
   }
 
+  void cmdVelPublishCallback(const ros::TimerEvent& event)
+  {
+
+    double totalTime =  callbackCount * callbackTime;
+    if(rotationCount == rotationsNeeded)
+    {
+      state = STOP;
+    }
+
+    else if(state == ROTATE && (totalTime >= rotationPeriod))
+    {
+      callbackCount = 0;
+      totalTime = 0;
+      rotationCount += 1;
+      state = WAIT;
+    }
+
+    else if(state == WAIT && (totalTime >= rotationPeriod / 20))
+    {
+      callbackCount = 0;
+      totalTime = 0;
+
+      state = ROTATE;
+    }
+
+    ROS_INFO_STREAM("total time"<< totalTime<<"rotationPeriod"<<rotationPeriod);
+    ROS_INFO_STREAM("call back count"<< callbackCount);
+    ROS_INFO_STREAM(state);
+
+    geometry_msgs::Twist rotationTwist;
+    rotationTwist.linear.x = 0;
+    rotationTwist.linear.y = 0;
+    rotationTwist.linear.z = 0;
+    rotationTwist.angular.x = 0;
+    rotationTwist.angular.y = 0;
+
+
+    switch(state)
+    {
+      case ROTATE: rotationTwist.angular.z = rotationVelocity;
+                   break;
+      case WAIT: rotationTwist.angular.z = 0;
+                 break;
+      case STOP: rotationTwist.angular.z = 0;
+                 break;
+    };
+
+    callbackCount += 1;
+
+    cmdVelPUblisher.publish(rotationTwist);
+  }
 };
 
 
 
-int main(int argc, char** argv )
+int main(int argc, char** argv)
 {
   RotateInPlace turtleRotate(argc, argv);
+  ros::NodeHandle n;
+  ros::Timer timer = n.createTimer(ros::Duration(callTime), &RotateInPlace::cmdVelPublishCallback,
+                                   &turtleRotate);
   ros::spin();
   return 0;
 }
